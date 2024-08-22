608cfd98126ff954d51624df4fe220765cff224b
Phil N <philipnduka92@gmail.com>
2024-08-22 08:20:29 +0100

    fix: setting ios

--- features/auth/test/auth_test.dart
+++ features/auth/test/auth_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:auth/auth.dart';
+// ignore: avoid_relative_lib_imports
+import '../lib/auth.dart';
+
+
 
 void main() {
   test('adds one to input values', () {
